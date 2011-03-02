ebfc9caba5b58b9e68c77af9c8e53f5562a2ee64
Anastasios Germanidis <agermanidis@gmail.com>
2011-03-02 18:24:07 -0500

    Packaged the deal

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "reddit",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = [],
+    author = "mellort",
+    description = "A Python wrapper for the Reddit API",
+    url = "https://github.com/mellort/reddit_api"
+)
+
