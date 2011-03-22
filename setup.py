cca10022172a72d68c29a7e10e3d81c61691cd8d
Timothy Mellor <timothy.mellor@gmail.com>
2011-03-22 12:55:48 -0400

    version number -> 1.0 :)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "reddit",
-    version = "0.1",
+    version = "1.0",
     packages = find_packages(),
     install_requires = [],
     author = "mellort",
