b309670583770dfc866de9c057e7d97eee91de0a
Timothy Mellor <timothy.mellor@gmail.com>
2011-04-14 14:30:11 -0400

    added gpl headers and COPYING file

--- reddit/comment.py
+++ reddit/comment.py
@@ -1,3 +1,18 @@
+# This file is part of reddit_api.
+# 
+# reddit_api is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# reddit_api is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with reddit_api.  If not, see <http://www.gnu.org/licenses/>.
+
 from features import Voteable, Deletable
 from base_objects import RedditContentObject
 from util import limit_chars
