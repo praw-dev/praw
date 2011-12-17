74bb962b3eefe04ce6acad88e6f53f43d10c8803
Bryce Boe <bbzbryce@gmail.com>
2011-12-16 16:50:28 -0800

    Add more complete meta-data to setup.py and performed a mini-version bump.

--- setup.py
+++ setup.py
@@ -6,12 +6,24 @@ except ImportError:
 
 setup(
     name='reddit',
-    version='1.1.1',
-    packages=['reddit'],
-    package_data={'reddit': ['*.cfg']},
-    author='mellort',
+    version='1.1.2',
+    author='Timothy Mellor',
     author_email='timothy.mellor+pip@gmail.com',
-    description='A Python wrapper for the Reddit API',
+    maintainer='Bryce Boe',
+    maintainer_email='bbzbryce+pip@gmail.com',
     url='https://github.com/mellort/reddit_api',
-    keywords=['reddit']
-)
+    description='A wrapper for the Reddit API',
+    long_description=('Please see the `documentation on github '
+                      '<https://github.com/mellort/reddit_api>`_.'),
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: GNU General Public License (GPL)',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2 :: Only',
+                 'Topic :: Utilities'],
+    license='GPLv3',
+    keywords=['api', 'reddit'],
+    packages=['reddit'],
+    package_data={'reddit': ['*.cfg']})
