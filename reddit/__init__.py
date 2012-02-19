72540ebb8e2aa3730c0a3ee88abeeb4a5ce3fc17
Mark Larus <larusm@kenyon.edu>
2012-02-18 23:32:38 -0500

    Removed import unicode_literals
    from __future__ import unicode_literals
    
    Was doing nothing useful.

--- reddit/__init__.py
+++ reddit/__init__.py
@@ -12,7 +12,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with reddit_api.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import unicode_literals
 
 try:
     import cookielib
