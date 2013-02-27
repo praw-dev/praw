ce64303d943ba75112e2032b65de3e0466dd1792
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-02-27 22:15:13 +0100

    Change Docstrings to follow Sphinx formatting.

--- praw/__init__.py
+++ praw/__init__.py
@@ -791,10 +791,10 @@ class AuthenticatedReddit(OAuth2Reddit, UnauthenticatedReddit):
 
         :param code: the code received in the request from the OAuth2 server
         :param update_session: Update the current session with the retrieved
-        token(s).
+            token(s).
         :returns: A dictionary with the key/value pairs for access_token,
-        refresh_token and scope. The refresh_token value will be done when
-        the OAuth2 grant is not refreshable.
+            refresh_token and scope. The refresh_token value will be done when
+            the OAuth2 grant is not refreshable.
 
         """
         retval = super(AuthenticatedReddit, self).get_access_information(code)
