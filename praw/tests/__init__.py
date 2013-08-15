3723f9aa036a6eb782e6ad05c6031af78f7dadf4
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-08-15 19:22:26 +0200

    Fix test_get_all_comments_gilded
    The gilded attribute for Comment objects is the number
    of times it has been gilded. If it is not gilded then this
    attribute will be 0.

--- praw/tests/__init__.py
+++ praw/tests/__init__.py
@@ -242,7 +242,7 @@ class BasicTest(unittest.TestCase, BasicHelper):
     def test_get_comments_gilded(self):
         gilded_comments = self.r.get_comments('all', gilded_only=True)
         for comment in gilded_comments:
-            self.assertTrue(comment.gilded >= 0)
+            self.assertTrue(comment.gilded > 0)
 
     @reddit_only
     def test_get_controversial(self):
