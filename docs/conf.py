e119c8f9d6150c367b2e40de36e7af907b6b266b
Shrayas <shrayasr@gmail.com>
2013-11-05 22:36:32 +0530

    RTD documentation moved to new theme
    * New theme looks way more clean and neat
    * Also mobile supported
    * Refer to
      http://ericholscher.com/blog/2013/nov/4/new-theme-read-the-docs/ for
      more information.

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+
+# setting it to default to allow RTD builds to pickup the new default theme
+html_theme = 'default'
+
+# setting an additional parameter to force using the new RTD theme
+RTD_NEW_THEME = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
