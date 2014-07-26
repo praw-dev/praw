praw-dev/praw

commit d0cf68c32d9a266bc77cf44ed04faf9ee0df8b19

Keep #311 fix compatible with python2x
The core of the issue is the changing of the variable
func_defaults to __defaults__. So we need six to keep it usable in
both versions of PRAW.

praw/decorators.py
@@ -291,8 +291,10 @@ def restrict_access(scope, mod=None, login=None, oauth_only=False):
                     # attribute to exist, thus it might not be set.
                     subreddit = cls if hasattr(cls, 'display_name') else False
                 else:
-                    subreddit = kwargs.get('subreddit', args[0] if args else
-                                           function.__defaults__[0])
+                    if args:
+                        subreddit = kwargs.get('subreddit', args[0])
+                    else:
+                        subreddit = six.get_function_defaults(function)[0]
             else:
                 subreddit = None
 

commit 3dbc6ac38c90c8a32eb2b8ac91bca212baf2f391

Produce consistent moderator checking results prior to function call.
Fixes the inconsistency that was introduced in
d0cf68c32d9a266bc77cf44ed04faf9ee0df8b19 between the following two calls:

    list([x.subreddit for x in r.get_mod_mail('foo', limit=10)])

    list([x.subreddit for x in r.get_mod_mail(subreddit='foo', limit=10)])

In the latter, PRAW would not check that the user was a moderator prior to the
function call because `subreddit` would always be set to `mod` by default.

praw/decorators.py
@@ -291,10 +291,9 @@ def restrict_access(scope, mod=None, login=None, oauth_only=False):
                     # attribute to exist, thus it might not be set.
                     subreddit = cls if hasattr(cls, 'display_name') else False
                 else:
-                    if args:
-                        subreddit = kwargs.get('subreddit', args[0])
-                    else:
-                        subreddit = six.get_function_defaults(function)[0]
+                    subreddit = kwargs.get(
+                        'subreddit', args[0] if args else
+                        six.get_function_defaults(function)[0])
             else:
                 subreddit = None
 