e5073e2d2d31cee4f64dd36607ae034db9fa16c5
Bryce Boe <bbzbryce@gmail.com>
2014-11-03 22:59:02 -0800

    Declare python 3.4 support.

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
                  'Programming Language :: Python :: 3.1',
                  'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
                  'Topic :: Utilities'],
     license='GPLv3',
     keywords='reddit api wrapper',
