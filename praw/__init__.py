c121dc1d4a70de97b8c4a0bf702fa0300e1186ac
Jonathan Cohen <jonathandcohen222@gmail.com>
2015-03-24 23:50:54 -0400

    Added hide_ads field to 'set_settings'
    json queries for subreddit settings include a "hide_ads" field which was not accounted for in set_settings().  Thus the pattern of grabbing a sub's settings, modifying them, and then resubmitting will throw a Warning because the hide_ads field grabbed from reddit gets caught in the **kwargs argument in set_settings()

--- praw/__init__.py
+++ praw/__init__.py
@@ -1490,7 +1490,7 @@ class ModConfigMixin(AuthenticatedReddit):
                      exclude_banned_modqueue=False, comment_score_hide_mins=0,
                      public_traffic=False, collapse_deleted_comments=False,
                      spam_comments='low', spam_links='high',
-                     spam_selfposts='high', submit_text='', **kwargs):
+                     spam_selfposts='high', submit_text='', hide_ads=False, **kwargs):
         """Set the settings for the given subreddit.
 
         :param subreddit: Must be a subreddit object.
@@ -1507,6 +1507,7 @@ class ModConfigMixin(AuthenticatedReddit):
                 'domain_sidebar': domain_sidebar,
                 'exclude_banned_modqueue': exclude_banned_modqueue,
                 'header-title': header_hover_text or '',
+                'hide_ads': hide_ads,
                 'lang': language,
                 'link_type': content_options,
                 'over_18': over_18,
