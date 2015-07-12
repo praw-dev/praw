6ab8ce2b9ae7304fbd9f99a2a0ae109f8e6617f4
Liudvikam <Liudvikam@users.noreply.github.com>
2015-07-12 09:31:31 +0300

    Fix flake8 and pep257 in decorators.py

--- praw/decorators.py
+++ praw/decorators.py
@@ -13,9 +13,12 @@
 # PRAW.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Decorators mainly do two things: ensure API guidelines are met and prevent unnecessary
-failed API requests by testing that the call can be made first. Also, they can limit the
-length of output strings and parse json response for certain errors.
+Decorators.
+
+They mainly do two things: ensure API guidelines are followed and
+prevent unnecessary failed API requests by testing that the call can be made
+first. Also, they can limit the length of output strings and parse json
+response for certain errors.
 """
 
 from __future__ import print_function, unicode_literals
@@ -272,7 +275,8 @@ def restrict_access(scope, mod=None, login=None, oauth_only=False):
 
     Returned data is not modified.
 
-    This decorator assumes that all mod required functions fit one of these categories:
+    This decorator assumes that all mod required functions fit one of these
+    categories:
 
       * have the subreddit as the first argument (Reddit instance functions) or
         have a subreddit keyword argument
