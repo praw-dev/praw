445364eaac8a5adec3dee4ea3952fc2405d8372b
13steinj <13steinj@users.noreply.github.com>
2015-08-27 19:32:44 -0400

    Test Hex-Edit
    Cross OS editing causes garbage on the beginning of files if
    automatically converting EOLs. Hex edit to remove said garbage, to make
    it work cross os.

--- praw/__init__.py
+++ praw/__init__.py
@@ -1,4 +1,4 @@
-﻿# This file is part of PRAW.
+# This file is part of PRAW.
 #
 # PRAW is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
