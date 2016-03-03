e9cf70fe4f6cb29b697637498d7777b5ce95aee1
Bryce Boe <bbzbryce@gmail.com>
2016-03-02 20:16:07 -0800

    Refactor how Config loads ini files and add tests.
    Ini files are no longer read on import, but when first needed.

--- praw/config.py
+++ praw/config.py
@@ -12,6 +12,27 @@ from .errors import ClientException
 class Config(object):
     """A class containing the configuration for a reddit site."""
 
+    CONFIG = None
+
+    @classmethod
+    def _load_config(cls):
+        """Attempt to load settings from various praw.ini files."""
+        config = configparser.RawConfigParser()
+        module_dir = os.path.dirname(sys.modules[__name__].__file__)
+        if 'APPDATA' in os.environ:  # Windows
+            os_config_path = os.environ['APPDATA']
+        elif 'XDG_CONFIG_HOME' in os.environ:  # Modern Linux
+            os_config_path = os.environ['XDG_CONFIG_HOME']
+        elif 'HOME' in os.environ:  # Legacy Linux
+            os_config_path = os.path.join(os.environ['HOME'], '.config')
+        else:
+            os_config_path = None
+        locations = [os.path.join(module_dir, 'praw.ini'), 'praw.ini']
+        if os_config_path is not None:
+            locations.insert(1, os.path.join(os_config_path, 'praw.ini'))
+        config.read(locations)
+        cls.CONFIG = config
+
     def __init__(self, site_name, **settings):
         """Initialize PRAW's configuration."""
         def config_boolean(item):
@@ -20,8 +41,10 @@ class Config(object):
         def fetch_or_none(key):
             return os.getenv(key) or obj.get(key) or None
 
-        obj = dict(CONFIG.items(site_name), **settings)
+        if Config.CONFIG is None:
+            self._load_config()
 
+        obj = dict(Config.CONFIG.items(site_name), **settings)
         self.by_kind = {obj['comment_kind']:    models.Comment,
                         obj['message_kind']:    models.Message,
                         obj['redditor_kind']:   models.Redditor,
@@ -59,26 +82,3 @@ class Config(object):
         if self._short_url is None:
             raise ClientException('No short domain specified.')
         return self._short_url
-
-
-def _load_configuration():
-    """Attempt to load settings from various praw.ini files."""
-    config = configparser.RawConfigParser()
-    module_dir = os.path.dirname(sys.modules[__name__].__file__)
-    if 'APPDATA' in os.environ:  # Windows
-        os_config_path = os.environ['APPDATA']
-    elif 'XDG_CONFIG_HOME' in os.environ:  # Modern Linux
-        os_config_path = os.environ['XDG_CONFIG_HOME']
-    elif 'HOME' in os.environ:  # Legacy Linux
-        os_config_path = os.path.join(os.environ['HOME'], '.config')
-    else:
-        os_config_path = None
-    locations = [os.path.join(module_dir, 'praw.ini'), 'praw.ini']
-    if os_config_path is not None:
-        locations.insert(1, os.path.join(os_config_path, 'praw.ini'))
-    if not config.read(locations):
-        raise Exception('Could not find config file in any of: {0}'
-                        .format(locations))
-    return config
-CONFIG = _load_configuration()
-del _load_configuration
---
+++ tests/unit/test_config.py
@@ -0,0 +1,76 @@
+import os
+import sys
+
+import mock
+import pytest
+from praw.config import Config
+from praw.errors import ClientException
+
+
+class TestConfig(object):
+    @staticmethod
+    def _assert_config_read(environment, mock_config):
+        mock_instance = mock_config.return_value
+        Config.CONFIG = None  # Force config file reload
+        prev_environment = os.environ.get(environment)
+        os.environ[environment] = '/MOCK'
+
+        module_dir = os.path.dirname(sys.modules['praw'].__file__)
+        environ_path = os.path.join(
+            '/MOCK', '.config' if environment == 'HOME' else '', 'praw.ini')
+        locations = [os.path.join(module_dir, 'praw.ini'), environ_path,
+                     'praw.ini']
+
+        try:
+            Config._load_config()
+            mock_instance.read.assert_called_with(locations)
+        finally:
+            Config.CONFIG = None  # Force config file reload
+            if prev_environment:
+                os.environ[environment] = prev_environment
+            else:
+                del os.environ[environment]
+
+    @mock.patch('six.moves.configparser.RawConfigParser')
+    def test_load_ini_from_appdata(self, mock_config):
+        self._assert_config_read('APPDATA', mock_config)
+
+    @mock.patch('six.moves.configparser.RawConfigParser')
+    def test_load_ini_from_home(self, mock_config):
+        self._assert_config_read('HOME', mock_config)
+
+    @mock.patch('six.moves.configparser.RawConfigParser')
+    def test_load_ini_from_xdg_config_home(self, mock_config):
+        self._assert_config_read('XDG_CONFIG_HOME', mock_config)
+
+    @mock.patch('six.moves.configparser.RawConfigParser')
+    def test_load_ini_with_no_config_directory(self, mock_config):
+        mock_instance = mock_config.return_value
+        Config.CONFIG = None  # Force config file reload
+
+        prev_environment = {}
+        for key in ['APPDATA', 'HOME', 'XDG_CONFIG_HOME']:
+            if key in os.environ:
+                prev_environment[key] = os.environ[key]
+                del os.environ[key]
+
+        module_dir = os.path.dirname(sys.modules['praw'].__file__)
+        locations = [os.path.join(module_dir, 'praw.ini'), 'praw.ini']
+
+        try:
+            Config._load_config()
+            mock_instance.read.assert_called_with(locations)
+        finally:
+            Config.CONFIG = None  # Force config file reload
+            for key, value in prev_environment.items():
+                os.environ[key] = value
+
+    def test_short_url(self):
+        config = Config('reddit')
+        assert config.short_url == 'https://redd.it'
+
+    def test_short_url_not_defined(self):
+        config = Config('reddit', short_url=None)
+        with pytest.raises(ClientException) as excinfo:
+            config.short_url
+        assert excinfo.value.message == 'No short domain specified.'
