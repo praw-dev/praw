92e257c275517c5733f252c487f7d579baff35a7
13steinj <13steinj@users.noreply.github.com>
2016-07-16 15:35:32 -0400

    Add tests for internal._warn_pyopenssl
    Add tests to patch in a pyOpenSSL version and ensure that the warnings are/are not triggered where applicable.

--- tests/test_internal.py
+++ tests/test_internal.py
@@ -1,6 +1,39 @@
 from __future__ import print_function, unicode_literals
+from six.moves import reload_module
+import sys
+from types import ModuleType
 from praw.internal import _to_reddit_list
 from .helper import PRAWTest, betamax
+
+
+def patch_openpyssl(version):
+    """Wrapper to patch a pyOpenSSL version.
+
+       - Calls the function with praw's internal module as the second argument.
+       - version defines the version string to patch in
+       - Reverts OpenSSL back to it's normal state after the function call if
+         it exists on the system, otherwise, deletes the patched module from
+         sys.modules
+    """
+    def factory(func):
+        def wrapped(obj):
+            try:
+                import OpenSSL as RealOpenSSL
+            except ImportError:
+                RealOpenSSL = None
+            FakeOpenSSL = ModuleType(str('openssl'))
+            FakeOpenSSL.__version__ = version
+            sys.modules['OpenSSL'] = FakeOpenSSL
+            from praw import internal
+            reload_module(internal)
+
+            retval = func(obj, internal)
+            del sys.modules['OpenSSL']
+            if RealOpenSSL is not None:
+                sys.modules['OpenSSL'] = RealOpenSSL
+            return retval
+        return wrapped
+    return factory
 
 
 class InternalTest(PRAWTest):
@@ -34,3 +67,16 @@ class InternalTest(PRAWTest):
         obj = self.r.get_subreddit(self.sr)
         output = _to_reddit_list([obj, 'hello'])
         self.assertEqual("{0},{1}".format(self.sr, 'hello'), output)
+
+    @patch_openpyssl('0.14')
+    def test__warn_pyopenssl_invalid_version(self, internal):
+        self.assertWarningsRegexp("0\.14", RuntimeWarning,
+                                  internal._warn_pyopenssl)
+
+    @patch_openpyssl('0.15')
+    def test__warn_pyopenssl_valid_version(self, internal):
+        self.assertNoWarnings(internal._warn_pyopenssl)
+
+    @patch_openpyssl('16.0.1.dev001')
+    def test__warn_pyopenssl_valid_version_dev(self, internal):
+        self.assertNoWarnings(internal._warn_pyopenssl)
