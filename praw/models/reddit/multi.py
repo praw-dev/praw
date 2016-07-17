78a5059dca1ff3bcfa7ebc50b423e933b015c78e
Bryce Boe <bbzbryce@gmail.com>
2016-07-17 07:07:20 -0700

    Add Multireddit.add and Multireddit.remove.

--- praw/models/reddit/multi.py
+++ praw/models/reddit/multi.py
@@ -25,21 +25,16 @@ class Multireddit(RedditBase, SubredditListingMixin):
         return API_PATH['multireddit_about'].format(multi=self.name,
                                                     user=self._author)
 
-    def add_subreddit(self, subreddit, _delete=False, *args, **kwargs):
-        """Add a subreddit to the multireddit.
+    def add(self, subreddit):
+        """Add a subreddit to this multireddit.
 
-        :param subreddit: The subreddit name or Subreddit object to add
-
-        The additional parameters are passed directly into
-        :meth:`~praw.__init__.BaseReddit.request_json`.
+        :param subreddit: The subreddit to add to this multi.
 
         """
-        subreddit = str(subreddit)
-        url = self._reddit.config['multireddit_add'].format(
-            user=self._author, multi=self.name, subreddit=subreddit)
-        method = 'DELETE' if _delete else 'PUT'
-        data = {'model': dumps({'name': subreddit})}
-        self._reddit.request(method, url, data=data, *args, **kwargs)
+        url = API_PATH['multireddit_update'].format(
+            multi=self.name, user=self._author, subreddit=subreddit)
+        self._reddit.request(
+            'put', url, data={'model': dumps({'name': str(subreddit)})})
 
     def copy(self, to_name):
         """Copy this multireddit.
@@ -71,9 +66,16 @@ class Multireddit(RedditBase, SubredditListingMixin):
         """
         return self._reddit.edit_multireddit(name=self.name, *args, **kwargs)
 
-    def remove_subreddit(self, subreddit, *args, **kwargs):
-        """Remove a subreddit from the user's multireddit."""
-        return self.add_subreddit(subreddit, True, *args, **kwargs)
+    def remove(self, subreddit):
+        """Remove a subreddit from this multireddit.
+
+        :param subreddit: The subreddit to remove from this multi.
+
+        """
+        url = API_PATH['multireddit_update'].format(
+            multi=self.name, user=self._author, subreddit=subreddit)
+        self._reddit.request(
+            'delete', url, data={'model': dumps({'name': str(subreddit)})})
 
     def rename(self, new_name, *args, **kwargs):
         """Rename this multireddit.
--- tests/integration/models/reddit/test_multi.py
+++ tests/integration/models/reddit/test_multi.py
@@ -1,9 +1,24 @@
 from praw.models import Subreddit
+import mock
 
 from ... import IntegrationTest
 
 
 class TestMultireddit(IntegrationTest):
+    @mock.patch('time.sleep', return_value=None)
+    def test_add(self, _):
+        self.reddit.read_only = False
+        with self.recorder.use_cassette('TestMulireddit.test_add'):
+            multi = self.reddit.user.multireddits()[0]
+            multi.add('redditdev')
+
+    @mock.patch('time.sleep', return_value=None)
+    def test_remove(self, _):
+        self.reddit.read_only = False
+        with self.recorder.use_cassette('TestMulireddit.test_remove'):
+            multi = self.reddit.user.multireddits()[0]
+            multi.remove('redditdev')
+
     def test_subreddits(self):
         multi = self.reddit.multireddit('kjoneslol', 'sfwpornnetwork')
         with self.recorder.use_cassette('TestMulireddit.test_subreddits'):
