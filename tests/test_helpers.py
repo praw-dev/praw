f3afbc369631e2797c4e2955ceb2074fe09e3cc9
13steinj <13steinj@users.noreply.github.com>
2016-07-20 23:40:48 -0400

    Test a comment stream

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,27 +1,39 @@
 """Tests for helper functions."""
 
 from __future__ import print_function, unicode_literals
-from praw.helpers import chunk_sequence
-from .helper import PRAWTest
+from praw import helpers, objects
+from .helper import PRAWTest, betamax
 
 
-class HeleperTest(PRAWTest):
+class HelperTest(PRAWTest):
+    def setUp(self):
+        super(HelperTest, self).setUp()
+        self.verbosity = 0
+
     def test_chunk_sequence(self):
         s = 'abcdefgh'
-        self.assertEqual(chunk_sequence(s, 4, allow_incomplete=True),
+        self.assertEqual(helpers.chunk_sequence(s, 4, allow_incomplete=True),
                          ['abcd', 'efgh'])
-        self.assertEqual(chunk_sequence(s, 3, allow_incomplete=True),
+        self.assertEqual(helpers.chunk_sequence(s, 3, allow_incomplete=True),
                          ['abc', 'def', 'gh'])
-        self.assertEqual(chunk_sequence(s, 3, allow_incomplete=False),
+        self.assertEqual(helpers.chunk_sequence(s, 3, allow_incomplete=False),
                          ['abc', 'def'])
-        self.assertEqual(chunk_sequence(s, 10, allow_incomplete=True),
+        self.assertEqual(helpers.chunk_sequence(s, 10, allow_incomplete=True),
                          ['abcdefgh'])
-        self.assertEqual(chunk_sequence(s, 10, allow_incomplete=False),
+        self.assertEqual(helpers.chunk_sequence(s, 10,
+                                                allow_incomplete=False),
                          [])
 
         # Returned sequence is same type as input
         s = ['a', 'b', 'c']
-        self.assertEqual(chunk_sequence(s, 2, allow_incomplete=True),
+        self.assertEqual(helpers.chunk_sequence(s, 2, allow_incomplete=True),
                          [['a', 'b'], ['c']])
-        self.assertEqual(chunk_sequence(s, 2, allow_incomplete=False),
+        self.assertEqual(helpers.chunk_sequence(s, 2, allow_incomplete=False),
                          [['a', 'b']])
+
+    @betamax()
+    def test_comment_stream(self):
+        generator = helpers.comment_stream(self.r, "all",
+                                           verbosity=self.verbosity)
+        for i in range(10001):
+            self.assertIsInstance(next(generator), objects.Comment)
