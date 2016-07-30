6f80fac31203b2276ecb5c518b2ca26a7196d698
Bryce Boe <bbzbryce@gmail.com>
2016-07-30 16:29:36 -0700

    Make vote private and update related docstring to be notes.

--- praw/models/reddit/mixins/votable.py
+++ praw/models/reddit/mixins/votable.py
@@ -5,56 +5,42 @@ from ....const import API_PATH
 class VotableMixin(object):
     """Interface for RedditBase classes that can be voted on."""
 
+    def _vote(self, direction):
+        self._reddit.post(API_PATH['vote'], data={'dir': str(direction),
+                                                  'id': self.fullname})
+
     def clear_vote(self):
         """Clear the authenticated user's vote on the object.
 
-        Note: votes must be cast by humans. That is, API clients proxying a
-        human's action one-for-one are OK, but bots deciding how to vote on
-        content or amplifying a human's vote are not. See the reddit rules for
-        more details on what constitutes vote cheating.
-
-        Source for note: http://www.reddit.com/dev/api#POST_api_vote
+        .. note:: votes must be cast by humans. That is, API clients proxying a
+           human's action one-for-one are OK, but bots deciding how to vote on
+           content or amplifying a human's vote are not. See the reddit rules
+           for more details on what constitutes vote cheating. [`Ref
+           <https://www.reddit.com/dev/api#POST_api_vote>`_]
 
         """
-        self.vote(direction=0)
+        self._vote(direction=0)
 
     def downvote(self):
         """Downvote the object.
 
-        Note: votes must be cast by humans. That is, API clients proxying a
-        human's action one-for-one are OK, but bots deciding how to vote on
-        content or amplifying a human's vote are not. See the reddit rules for
-        more details on what constitutes vote cheating.
-
-        Source for note: http://www.reddit.com/dev/api#POST_api_vote
+        .. note:: votes must be cast by humans. That is, API clients proxying a
+           human's action one-for-one are OK, but bots deciding how to vote on
+           content or amplifying a human's vote are not. See the reddit rules
+           for more details on what constitutes vote cheating. [`Ref
+           <https://www.reddit.com/dev/api#POST_api_vote>`_]
 
         """
-        self.vote(direction=-1)
+        self._vote(direction=-1)
 
     def upvote(self):
         """Upvote the object.
 
-        Note: votes must be cast by humans. That is, API clients proxying a
-        human's action one-for-one are OK, but bots deciding how to vote on
-        content or amplifying a human's vote are not. See the reddit rules for
-        more details on what constitutes vote cheating.
+        .. note:: votes must be cast by humans. That is, API clients proxying a
+           human's action one-for-one are OK, but bots deciding how to vote on
+           content or amplifying a human's vote are not. See the reddit rules
+           for more details on what constitutes vote cheating. [`Ref
+           <https://www.reddit.com/dev/api#POST_api_vote>`_]
 
         """
-        self.vote(direction=1)
-
-    def vote(self, direction):
-        """Vote on the object in the direction specified.
-
-        :param direction: The direction to vote in with -1 being down; 0,
-            clear; and 1, up.
-
-        Note: votes must be cast by humans. That is, API clients proxying a
-        human's action one-for-one are OK, but bots deciding how to vote on
-        content or amplifying a human's vote are not. See the reddit rules for
-        more details on what constitutes vote cheating.
-
-        Source for note: http://www.reddit.com/dev/api#POST_api_vote
-
-        """
-        self._reddit.post(API_PATH['vote'], data={'dir': str(direction),
-                                                  'id': self.fullname})
+        self._vote(direction=1)
