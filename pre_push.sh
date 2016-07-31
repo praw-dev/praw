01bae5e2b554c257442d19888eb32a0a291af3c9
Bryce Boe <bbzbryce@gmail.com>
2016-07-30 23:27:51 -0700

    Add sphinx building as part of the test process.

--- pre_push.sh
+++ pre_push.sh
@@ -6,14 +6,22 @@ dir=$(dirname $0)
 flake8 --exclude=docs
 if [ $? -ne 0 ]; then
     echo "Exiting due to flake8 errors. Fix and re-run to finish tests."
-    exit $?
+    exit 1
 fi
 
 # pydocstyle
 pydocstyle praw
 if [ $? -ne 0 ]; then
     echo "Exiting due to pydocstyle errors. Fix and re-run to finish tests."
-    exit $?
+    exit 1
+fi
+
+# sphinx
+rm -rf /tmp/_praw_sphinx 2> /dev/null
+sphinx-build -W docs/ /tmp/_praw_sphinx
+if [ $? -ne 0 ]; then
+    echo "Exiting due to sphinx-buld errors. Fix and re-run to finish tests."
+    exit 1
 fi
 
 # pylint
@@ -21,6 +29,7 @@ output=$(pylint --rcfile=$dir/.pylintrc $dir/praw 2> /dev/null)
 if [ -n "$output" ]; then
     echo "--pylint--"
     echo -e "$output"
+    exit 1
 fi
 
 
