4c6ec556f7f243b24a74489ef2ab4cbccd275cf7
Bryce Boe <bbzbryce@gmail.com>
2016-11-25 09:24:29 -0800

    Use sphinx_rtd_theme locally and update copyright year.

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,17 @@
+import os
 import sys
 sys.path.insert(0, "..")
 
 from praw import __version__
 
-copyright = '2014, Bryce Boe'
+copyright = '2016, Bryce Boe'
 exclude_patterns = ['_build']
 extensions = ['sphinx.ext.autodoc']
 html_static_path = ['_static']
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False
+}
 html_use_smartypants = True
 htmlhelp_basename = 'PRAW'
 master_doc = 'index'
@@ -18,6 +22,12 @@ release = __version__
 source_suffix = '.rst'
 suppress_warnings = ['image.nonlocal_uri']
 version = '.'.join(__version__.split('.', 2)[:2])
+
+
+# Use RTD theme locally
+if not os.environ.get('READTHEDOCS'):
+    import sphinx_rtd_theme
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 def skip(app, what, name, obj, skip, options):
