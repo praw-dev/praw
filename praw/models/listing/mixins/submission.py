7b4b5bc95f0a498ab83422192410f9213bfdb251
Bryce Boe <bbzbryce@gmail.com>
2016-12-17 00:07:37 -0800

    Remove BaseListingMixin as superclass for SubmissionListingMixin

--- praw/models/listing/mixins/submission.py
+++ praw/models/listing/mixins/submission.py
@@ -1,11 +1,10 @@
 """Provide the SubmissionListingMixin class."""
 from ....const import API_PATH
 from ..generator import ListingGenerator
-from .base import BaseListingMixin
 from .gilded import GildedListingMixin
 
 
-class SubmissionListingMixin(BaseListingMixin, GildedListingMixin):
+class SubmissionListingMixin(GildedListingMixin):
     """Adds additional methods pertaining to Submission instances."""
 
     def duplicates(self, **generator_kwargs):
