acf6b198c6d68b91ed0d24a4e2f8cc771b4cf49f
Bryce Boe <bbzbryce@gmail.com>
2017-05-24 16:27:45 -0700

    Clarify when inboxable methods can be used

--- praw/models/reddit/mixins/inboxable.py
+++ praw/models/reddit/mixins/inboxable.py
@@ -10,15 +10,26 @@ class InboxableMixin(object):
         """Block the user who sent the item.
 
         .. note:: Reddit does not permit blocking users unless you have a
-                  :class:`.Comment` or :class:`.Message` from them.
+                  :class:`.Comment` or :class:`.Message` from them in your
+                  inbox.
 
         """
         self._reddit.post(API_PATH['block'], data={'id': self.fullname})
 
     def mark_read(self):
-        """Mark the item as read."""
+        """Mark the item as read.
+
+        .. note:: This method pertains only to objects which were retrieved via
+                  the inbox.
+
+        """
         self._reddit.inbox.mark_read([self])
 
     def mark_unread(self):
-        """Mark the item as unread."""
+        """Mark the item as unread.
+
+        .. note:: This method pertains only to objects which were retrieved via
+                  the inbox.
+
+        """
         self._reddit.inbox.mark_unread([self])
