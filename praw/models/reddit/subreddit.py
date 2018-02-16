fae1f8406802842b6303c8a224d37f6888d183ec
bakonydraco <bakonydraco1@gmail.com>
2018-02-16 10:39:43 -0800

    Fixed whitespace

--- praw/models/reddit/subreddit.py
+++ praw/models/reddit/subreddit.py
@@ -2096,23 +2096,23 @@ class SubredditWiki(object):
 
     def __getitem__(self, page_name):
         """Lazily return the WikiPage for the subreddit named ``page_name``.
-        
+
         This method is to be used to fetch a specific wikipage, like so:
-        
+
         .. code:: python
-        
+
            wikipage = reddit.subreddit('iama').wiki['proof']
            print(wikipage.content_md)
-           
+
         """
         return WikiPage(self.subreddit._reddit, self.subreddit,
                         page_name.lower())
 
     def __init__(self, subreddit):
         """Create a SubredditModeration instance.
-        
+
         :param subreddit: The subreddit to moderate.
-        
+
         """
         self.banned = SubredditRelationship(subreddit, 'wikibanned')
         self.contributor = SubredditRelationship(subreddit, 'wikicontributor')
@@ -2120,14 +2120,14 @@ class SubredditWiki(object):
 
     def __iter__(self):
         """Iterate through the pages of the wiki.
-        
+
         This method is to be used to discover all wikipages for a subreddit:
-        
+
         .. code:: python
-        
+
            for wikipage in reddit.subreddit('iama').wiki:
                print(wikipage)
-               
+
         """
         response = self.subreddit._reddit.get(
             API_PATH['wiki_pages'].format(subreddit=self.subreddit),
@@ -2137,20 +2137,20 @@ class SubredditWiki(object):
 
     def create(self, name, content, reason=None, **other_settings):
         """Create a new wiki page.
-        
+
         :param name: The name of the new WikiPage. This name will be
             normalized.
         :param content: The content of the new WikiPage.
         :param reason: (Optional) The reason for the creation.
         :param other_settings: Additional keyword arguments to pass.
-        
+
         To create the wiki page ``'praw_test'`` in ``'/r/test'`` try:
-        
+
         .. code:: python
-        
+
            reddit.subreddit('test').wiki.create(
                'praw_test', 'wiki body text', reason='PRAW Test Creation')
-               
+
         """
         name = name.replace(' ', '_').lower()
         new = WikiPage(self.subreddit._reddit, self.subreddit, name)
@@ -2159,17 +2159,17 @@ class SubredditWiki(object):
 
     def revisions(self, **generator_kwargs):
         """Return a generator for recent wiki revisions.
-        
+
         Additional keyword arguments are passed in the initialization of
         :class:`.ListingGenerator`.
-        
+
         To view the wiki revisions for ``'praw_test'`` in ``'/r/test'`` try:
-        
+
         .. code:: python
-        
+
            for item in reddit.subreddit('test').wiki['praw_test'].revisions():
                print(item)
-               
+
         """
         url = API_PATH['wiki_revisions'].format(subreddit=self.subreddit)
         return WikiPage._revision_generator(
