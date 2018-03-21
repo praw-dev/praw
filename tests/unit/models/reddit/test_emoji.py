c5075ee2c2b0cff52e1c4554c68a552718779431
bakonydraco <bakonydraco1@gmail.com>
2018-03-20 23:33:55 -0700

    Fixed issues

--- tests/unit/models/reddit/test_emoji.py
+++ tests/unit/models/reddit/test_emoji.py
@@ -8,13 +8,13 @@ from ... import UnitTest
 class TestEmoji(UnitTest):
     def test_equality(self):
         emoji1 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'a'),
-                         name='x')
+                       name='x')
         emoji2 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'a'),
-                         name='2')
+                       name='2')
         emoji3 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'b'),
-                         name='1')
+                       name='1')
         emoji4 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'A'),
-                         name='x')
+                       name='x')
         assert emoji1 == emoji1
         assert emoji2 == emoji2
         assert emoji3 == emoji3
@@ -24,13 +24,13 @@ class TestEmoji(UnitTest):
 
     def test_hash(self):
         emoji1 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'a'),
-                         name='x')
+                       name='x')
         emoji2 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'a'),
-                         name='2')
+                       name='2')
         emoji3 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'b'),
-                         name='1')
+                       name='1')
         emoji4 = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'A'),
-                         name='x')
+                       name='x')
         assert hash(emoji1) == hash(emoji1)
         assert hash(emoji2) == hash(emoji2)
         assert hash(emoji3) == hash(emoji3)
@@ -40,19 +40,18 @@ class TestEmoji(UnitTest):
 
     def test_pickle(self):
         emoji = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'a'),
-                        name='x')
+                      name='x')
         for level in range(pickle.HIGHEST_PROTOCOL + 1):
-            other = pickle.loads(pickle.dumps(page, protocol=level))
+            other = pickle.loads(pickle.dumps(emoji, protocol=level))
             assert page == other
 
     def test_repr(self):
         emoji = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'a'),
-                        name='x')
-        assert repr(page) == ('Emoji(subreddit=Subreddit(display_name=\'a\''
+                      name='x')
+        assert repr(emoji) == ('Emoji(subreddit=Subreddit(display_name=\'a\''
                               '), name=\'x\')')
 
     def test_str(self):
         emoji = Emoji(self.reddit, subreddit=Subreddit(self.reddit, 'a'),
-                        name='x')
-        assert str(page) == 'a/x'
-
+                      name='x')
+        assert str(emoji) == 'a/x'
