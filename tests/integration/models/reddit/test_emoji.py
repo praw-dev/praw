e93a9ba6b9bd78186af6c88e3add52d71c5e09d0
bakonydraco <bakonydraco1@gmail.com>
2018-03-22 22:54:16 -0700

    fixed test

--- tests/integration/models/reddit/test_emoji.py
+++ tests/integration/models/reddit/test_emoji.py
@@ -8,17 +8,17 @@ class TestEmojiModeration(IntegrationTest):
     def test_add(self):
         subreddit = self.reddit.subreddit(
             pytest.placeholders.test_subreddit)
-        page = Emoji(self.reddit, subreddit, 'test')
+        emoji = Emoji(self.reddit, subreddit, 'test')
 
         self.reddit.read_only = False
         with self.recorder.use_cassette('TestEmojiModeration.test_add'):
-            subreddit.emoji['test'].add('tests/integration/files/test.png')
+            emoji.add('tests/integration/files/test.png')
 
     def test_remove(self):
         subreddit = self.reddit.subreddit(
             pytest.placeholders.test_subreddit)
-        page = Emoji(self.reddit, subreddit, 'test')
+        emoji = Emoji(self.reddit, subreddit, 'test')
 
         self.reddit.read_only = False
         with self.recorder.use_cassette('TestEmojiModeration.test_remove'):
-            subreddit.emoji['test'].remove()
+            emoji.remove()
