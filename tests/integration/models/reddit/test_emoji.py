9c62988a1cc2bbaf9a223eece1a5150aa9f14ebb
bakonydraco <bakonydraco1@gmail.com>
2018-03-22 18:17:01 -0700

    Add files via upload

---
+++ tests/integration/models/reddit/test_emoji.py
@@ -0,0 +1,26 @@
+from praw.models import Redditor, Emoji
+from prawcore import NotFound
+import mock
+import pytest
+
+from ... import IntegrationTest
+
+
+class TestEmojiModeration(IntegrationTest):
+    def test_add(self):
+        subreddit = self.reddit.subreddit(
+            pytest.placeholders.test_subreddit)
+        page = Emoji(self.reddit, subreddit, 'test')
+
+        self.reddit.read_only = False
+        with self.recorder.use_cassette('TestEmojiModeration.test_add'):
+            subreddit.emoji['test'].add('../../files/test.png')
+
+    def test_remove(self):
+        subreddit = self.reddit.subreddit(
+            pytest.placeholders.test_subreddit)
+        page = Emoji(self.reddit, subreddit, 'test')
+
+        self.reddit.read_only = False
+        with self.recorder.use_cassette('TestEmojiModeration.test_remove'):
+            subreddit.emoji['test'].remove()
