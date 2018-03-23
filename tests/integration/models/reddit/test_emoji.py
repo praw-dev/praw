813c4e6dd4575b67c25597faeae56927ae93092f
bakonydraco <bakonydraco1@gmail.com>
2018-03-22 23:24:49 -0700

    Added jpg support

--- tests/integration/models/reddit/test_emoji.py
+++ tests/integration/models/reddit/test_emoji.py
@@ -7,20 +7,22 @@ from ... import IntegrationTest
 class TestEmojiModeration(IntegrationTest):
     @mock.patch('time.sleep', return_value=None)
     def test_add(self, _):
+        self.reddit.read_only = False
         subreddit = self.reddit.subreddit(
             pytest.placeholders.test_subreddit)
-        emoji = Emoji(self.reddit, subreddit, 'test')
-
-        self.reddit.read_only = False
+        emoji_png = Emoji(self.reddit, subreddit, 'test_png')
+        emoji_jpg = Emoji(self.reddit, subreddit, 'test_jpg')
         with self.recorder.use_cassette('TestEmojiModeration.test_add'):
-            emoji.add('tests/integration/files/test.png')
+            emoji_png.add('tests/integration/files/test.png')
+            emoji_jpg.add('tests/integration/files/test.jpg')
 
     @mock.patch('time.sleep', return_value=None)
     def test_remove(self, _):
+        self.reddit.read_only = False
         subreddit = self.reddit.subreddit(
             pytest.placeholders.test_subreddit)
-        emoji = Emoji(self.reddit, subreddit, 'test')
-
-        self.reddit.read_only = False
+        emoji_png = Emoji(self.reddit, subreddit, 'test_png')
+        emoji_jpg = Emoji(self.reddit, subreddit, 'test_jpg')
         with self.recorder.use_cassette('TestEmojiModeration.test_remove'):
-            emoji.remove()
+            emoji_png.remove()
+            emoji_jpg.remove()
