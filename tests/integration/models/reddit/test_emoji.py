2c8c570efb4434efe90cb6d3686a8805d1f088fb
bakonydraco <bakonydraco1@gmail.com>
2018-03-23 01:57:19 -0700

    Fixed per changes

--- tests/integration/models/reddit/test_emoji.py
+++ tests/integration/models/reddit/test_emoji.py
@@ -1,28 +1,16 @@
 from praw.models import Emoji
 import pytest
 import mock
+
 from ... import IntegrationTest
 
 
-class TestEmojiModeration(IntegrationTest):
-    @mock.patch('time.sleep', return_value=None)
-    def test_add(self, _):
-        self.reddit.read_only = False
-        subreddit = self.reddit.subreddit(
-            pytest.placeholders.test_subreddit)
-        emoji_png = Emoji(self.reddit, subreddit, 'test_png')
-        emoji_jpg = Emoji(self.reddit, subreddit, 'test_jpg')
-        with self.recorder.use_cassette('TestEmojiModeration.test_add'):
-            emoji_png.add('tests/integration/files/test.png')
-            emoji_jpg.add('tests/integration/files/test.jpg')
-
+class TestEmoji(IntegrationTest):
     @mock.patch('time.sleep', return_value=None)
     def test_remove(self, _):
         self.reddit.read_only = False
         subreddit = self.reddit.subreddit(
             pytest.placeholders.test_subreddit)
-        emoji_png = Emoji(self.reddit, subreddit, 'test_png')
-        emoji_jpg = Emoji(self.reddit, subreddit, 'test_jpg')
         with self.recorder.use_cassette('TestEmojiModeration.test_remove'):
-            emoji_png.remove()
-            emoji_jpg.remove()
+            subreddit.emoji['test_png'].remove()
+            subreddit.emoji['test_jpg'].remove()
