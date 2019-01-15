e82fcca297526bcc4ee3d3daf9f5f8f02aae8fd3
jarhill0 <jarhill0@users.noreply.github.com>
2019-01-14 17:07:40 -0800

    Document typical Trophy attributes

--- praw/models/trophy.py
+++ praw/models/trophy.py
@@ -9,6 +9,25 @@ class Trophy(PRAWBase):
     :meth:`.Redditor.trophies` can be used to get a list
     of the redditor's trophies.
 
+
+    **Typical Attributes**
+
+    This table describes attributes that typically belong to objects of this
+    class. Since attributes are dynamically provided (see
+    :ref:`determine-available-attributes-of-an-object`), there is not a
+    guarantee that these attributes will always be present, nor is this list
+    necessarily comprehensive.
+
+    ======================= ===================================================
+    Attribute               Description
+    ======================= ===================================================
+    ``award_id``            The ID of the trophy (sometimes ``None``).
+    ``description``         The description of the trophy (sometimes ``None``).
+    ``icon_40``             The URL of a 41x41 px icon for the trophy.
+    ``icon_70``             The URL of a 71x71 px icon for the trophy.
+    ``name``                The name of the trophy.
+    ``url``                 A relevant URL (sometimes ``None``).
+    ======================= ===================================================
     """
 
     def __init__(self, reddit, _data):
