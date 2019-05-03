f4f5fbdbad6f7c2c2b2aa3b827502c3dbad74edd
Pyprohly <pyprohly@outlook.com>
2019-05-04 01:29:08 +1000

    Tweak const.py

--- praw/const.py
+++ praw/const.py
@@ -1,13 +1,14 @@
-"""praw constants."""
+"""PRAW constants."""
 
-from .endpoints import API_PATH
+from .endpoints import API_PATH  # noqa: F401
 
 __version__ = "6.1.2.dev0"
 
-JPEG_HEADER = b"\xff\xd8\xff"
-MAX_IMAGE_SIZE = 512000
-MIN_PNG_SIZE = 67
-MIN_JPEG_SIZE = 128
-PNG_HEADER = b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a"
+USER_AGENT_FORMAT = "{} PRAW/" + __version__
 
-USER_AGENT_FORMAT = "{{}} PRAW/{}".format(__version__)
+MAX_IMAGE_SIZE = 512000
+MIN_JPEG_SIZE = 128
+MIN_PNG_SIZE = 67
+
+JPEG_HEADER = b"\xff\xd8\xff"
+PNG_HEADER = b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a"
