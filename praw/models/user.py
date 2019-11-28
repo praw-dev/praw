3238a0b763cf5c090d264ed2bcb2d6a5ec38dfea
kungming2 <24950347+kungming2@users.noreply.github.com>
2019-11-28 12:53:08 -0800

    Documented moderator_subreddits's limit of 100

--- praw/models/user.py
+++ praw/models/user.py
@@ -105,6 +105,15 @@ class User(PRAWBase):
         Additional keyword arguments are passed in the initialization of
         :class:`.ListingGenerator`.
 
+        .. note:: This method will return a maximum of 100 moderated 
+        subreddits, ordered by subscriber count. To retrieve more than
+        100 subreddits, use :meth:`.Redditor.moderated`.
+        
+        .. code-block:: python
+        
+                for subreddit in reddit.user.moderator_subreddits():
+                    print(subreddit.display_name)
+
         """
         return ListingGenerator(
             self._reddit, API_PATH["my_moderator"], **generator_kwargs
