d734e17bfc792b8a22a10d617363888d7daa1eff
fwump38 <fwump38@gmail.com>
2019-12-01 15:08:02 -0500

    renamed classes for removal reasons

--- praw/models/__init__.py
+++ praw/models/__init__.py
@@ -18,8 +18,8 @@ from .reddit.message import Message, SubredditMessage
 from .reddit.modmail import ModmailAction, ModmailConversation, ModmailMessage
 from .reddit.more import MoreComments
 from .reddit.multi import Multireddit
-from .reddit.reasons import Reason
 from .reddit.redditor import Redditor
+from .reddit.removal_reasons import RemovalReason
 from .reddit.submission import Submission
 from .reddit.subreddit import Subreddit
 from .reddit.widgets import (
