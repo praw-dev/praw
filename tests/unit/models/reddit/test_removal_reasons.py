ea82a1136c662914b5efa9b61368a8f0cdb789ca
fwump38 <fwump38@gmail.com>
2019-12-01 15:06:24 -0500

    renamed test files to match module name

--- tests/unit/models/reddit/test_removal_reasons.py
+++ tests/unit/models/reddit/test_removal_reasons.py
@@ -1,30 +1,27 @@
 import pickle
 
-from praw.models import Subreddit, Reason
-from praw.models.reddit.reasons import SubredditReasons
+from praw.models import Subreddit, RemovalReason
+from praw.models.reddit.removal_reasons import SubredditRemovalReasons
 
 from ... import UnitTest
 
 
-class TestReason(UnitTest):
+class TestRemovalReason(UnitTest):
     def test_equality(self):
-        reason1 = Reason(
+        reason1 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="x"
         )
-        reason2 = Reason(
+        reason2 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="2"
         )
-        reason3 = Reason(
+        reason3 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "b"), reason_id="1"
         )
-        reason4 = Reason(
+        reason4 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "A"), reason_id="x"
         )
-        reason5 = Reason(
+        reason5 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="X"
-        )
-        reason6 = Reason(
-            self.reddit, subreddit=Subreddit(self.reddit, "b"), reason_id="x"
         )
         assert reason1 == reason1
         assert reason1 == "x"
@@ -34,31 +31,27 @@ class TestReason(UnitTest):
         assert reason1 != reason3
         assert reason1 == reason4
         assert reason1 != reason5
-        assert reason1 != reason6
 
     def test__get(self):
         subreddit = self.reddit.subreddit("a")
-        reason = subreddit.reasons["a"]
-        assert isinstance(reason, Reason)
+        removal_reason = subreddit.mod.removal_reasons["a"]
+        assert isinstance(removal_reason, RemovalReason)
 
     def test_hash(self):
-        reason1 = Reason(
+        reason1 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="x"
         )
-        reason2 = Reason(
+        reason2 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="2"
         )
-        reason3 = Reason(
+        reason3 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "b"), reason_id="1"
         )
-        reason4 = Reason(
+        reason4 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "A"), reason_id="x"
         )
-        reason5 = Reason(
+        reason5 = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="X"
-        )
-        reason6 = Reason(
-            self.reddit, subreddit=Subreddit(self.reddit, "b"), reason_id="x"
         )
         assert hash(reason1) == hash(reason1)
         assert hash(reason2) == hash(reason2)
@@ -67,10 +60,9 @@ class TestReason(UnitTest):
         assert hash(reason1) != hash(reason3)
         assert hash(reason1) == hash(reason4)
         assert hash(reason1) != hash(reason5)
-        assert hash(reason1) != hash(reason6)
 
     def test_pickle(self):
-        reason = Reason(
+        reason = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="x"
         )
         for level in range(pickle.HIGHEST_PROTOCOL + 1):
@@ -78,19 +70,19 @@ class TestReason(UnitTest):
             assert reason == other
 
     def test_repr(self):
-        reason = Reason(
+        reason = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="x"
         )
-        assert repr(reason) == ("Reason(id='x')")
+        assert repr(reason) == ("RemovalReason(id='x')")
 
     def test_str(self):
-        reason = Reason(
+        reason = RemovalReason(
             self.reddit, subreddit=Subreddit(self.reddit, "a"), reason_id="x"
         )
         assert str(reason) == "x"
 
 
-class TestSubredditReasons(UnitTest):
+class TestSubredditRemovalReasons(UnitTest):
     def test_repr(self):
-        sr = SubredditReasons(subreddit=Subreddit(self.reddit, "a"))
+        sr = SubredditRemovalReasons(subreddit=Subreddit(self.reddit, "a"))
         assert repr(sr)  # assert it has some repr
