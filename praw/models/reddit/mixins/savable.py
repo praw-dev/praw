411c67b7a506b8d9e1b9ce7503ceb055e50c6416
kungming2 <24950347+kungming2@users.noreply.github.com>
2019-12-05 23:21:55 -0800

    Update savable.py

--- praw/models/reddit/mixins/savable.py
+++ praw/models/reddit/mixins/savable.py
@@ -8,8 +8,9 @@ class SavableMixin:
     def save(self, category=None):
         """Save the object.
 
-        :param category: (Premium) The category to save to. If your user does not
-            have Reddit Premium this value is ignored by Reddit (default: None).
+        :param category: (Premium) The category to save to. If your user does
+            not have Reddit Premium this value is ignored by Reddit
+            (default: ``None``).
 
         Example usage:
 
