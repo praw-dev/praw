5a4c85a6bccf73d547dae0e1ebc3e30ddf4c0edc
PokestarFan <sarkaraoyan@gmail.com>
2019-12-22 19:41:12 -0500

    Add stubs for praw/const.py

--- stubs/praw/const.pyi
+++ stubs/praw/const.pyi
@@ -1,8 +1,5 @@
-from .endpoints import API_PATH as API_PATH
-from typing import Any
-
 __version__: str
-USER_AGENT_FORMAT: Any
+USER_AGENT_FORMAT: str
 MAX_IMAGE_SIZE: int
 MIN_JPEG_SIZE: int
 MIN_PNG_SIZE: int
