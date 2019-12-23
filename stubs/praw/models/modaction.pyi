f67d82f6277757127fa81db0684c9c4af881ad51
PokestarFan <sarkaraoyan@gmail.com>
2019-12-22 22:26:39 -0500

    Stub praw/models/modaction

--- stubs/praw/models/modaction.pyi
+++ stubs/praw/models/modaction.pyi
@@ -1,10 +1,11 @@
-from typing import Any
+from typing import Any, NoReturn
 
 from .base import PRAWBase as PRAWBase
+from .reddit.redditor import Redditor
 
 
 class ModAction(PRAWBase):
     @property
-    def mod(self): ...
+    def mod(self) -> Redditor: ...
     @mod.setter
-    def mod(self, value: Any) -> None: ...
+    def mod(self, value: Any) -> NoReturn: ...
