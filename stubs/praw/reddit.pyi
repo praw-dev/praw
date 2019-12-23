5138f7020cf614588581b788a2d2acd5290999b0
PokestarFan <sarkaraoyan@gmail.com>
2019-12-22 21:35:52 -0500

    Reformat reddit

--- stubs/praw/reddit.pyi
+++ stubs/praw/reddit.pyi
@@ -1,13 +1,35 @@
-from typing import Any, Optional, NoReturn, Dict, Union, Sequence, \
-    Generator, IO
+from typing import (
+    Any,
+    Optional,
+    NoReturn,
+    Dict,
+    Union,
+    Sequence,
+    Generator,
+    IO,
+)
 
 from prawcore import Requestor
 
 from .config import Config as Config
-from .models import Auth, Front, Inbox, LiveHelper, MultiredditHelper, \
-    Redditors, SubredditHelper, Subreddits
-from .models import User, Comment, Subreddit, Redditor, DomainListing, \
-    Submission
+from .models import (
+    Auth,
+    Front,
+    Inbox,
+    LiveHelper,
+    MultiredditHelper,
+    Redditors,
+    SubredditHelper,
+    Subreddits,
+)
+from .models import (
+    User,
+    Comment,
+    Subreddit,
+    Redditor,
+    DomainListing,
+    Submission,
+)
 from .models.reddit.base import RedditBase
 
 UPDATE_CHECKER_MISSING: bool
@@ -30,15 +52,64 @@ class Reddit:
     subreddit: SubredditHelper = ...
     subreddits: Subreddits = ...
     user: User = ...
-    def __init__(self, site_name: Optional[str] = ..., requestor_class: Optional[Requestor] = ..., requestor_kwargs: Optional[Dict[str, Union[str, Any]]] = ..., **config_settings: Any) -> None: ...
-    def comment(self, id: Optional[str] = ..., url: Optional[str] = ...) -> Comment: ...
+    def __init__(
+        self,
+        site_name: Optional[str] = ...,
+        requestor_class: Optional[Requestor] = ...,
+        requestor_kwargs: Optional[Dict[str, Union[str, Any]]] = ...,
+        **config_settings: Any
+    ) -> None: ...
+    def comment(
+        self, id: Optional[str] = ..., url: Optional[str] = ...
+    ) -> Comment: ...
     def domain(self, domain: str) -> DomainListing: ...
-    def get(self, path: str, params: Optional[Union[Dict[str, str], str]] = ...) -> Optional[RedditBase]: ...
-    def info(self, fullnames: Optional[Sequence[Union[Comment, Subreddit, Submission]]] = ..., url: Optional[str] = ...) -> Generator[Any]: ...
-    def patch(self, path: str, data: Optional[Union[Dict[str, Union[str, Any]], bytes, IO, str]] = ...) -> Optional[RedditBase]: ...
-    def post(self, path: str, data: Optional[Union[Dict[str, Union[str, Any]], bytes, IO, str]] = ..., files: Optional[Dict[str, IO]] = ..., params: Optional[Union[str, Dict[str, str]]] = ...) -> Optional[RedditBase]: ...
-    def put(self, path: str, data: Optional[Union[Dict[str, Union[str, Any]], bytes, IO, str]] = ...) -> Optional[RedditBase]: ...
+    def get(
+        self, path: str, params: Optional[Union[Dict[str, str], str]] = ...
+    ) -> Optional[RedditBase]: ...
+    def info(
+        self,
+        fullnames: Optional[
+            Sequence[Union[Comment, Subreddit, Submission]]
+        ] = ...,
+        url: Optional[str] = ...,
+    ) -> Generator[Any]: ...
+    def patch(
+        self,
+        path: str,
+        data: Optional[
+            Union[Dict[str, Union[str, Any]], bytes, IO, str]
+        ] = ...,
+    ) -> Optional[RedditBase]: ...
+    def post(
+        self,
+        path: str,
+        data: Optional[
+            Union[Dict[str, Union[str, Any]], bytes, IO, str]
+        ] = ...,
+        files: Optional[Dict[str, IO]] = ...,
+        params: Optional[Union[str, Dict[str, str]]] = ...,
+    ) -> Optional[RedditBase]: ...
+    def put(
+        self,
+        path: str,
+        data: Optional[
+            Union[Dict[str, Union[str, Any]], bytes, IO, str]
+        ] = ...,
+    ) -> Optional[RedditBase]: ...
     def random_subreddit(self, nsfw: bool = ...) -> Subreddit: ...
-    def redditor(self, name: Optional[str] = ..., fullname: Optional[str] = ...) -> Redditor: ...
-    def request(self, method: str, path: str, params: Optional[Union[str, Dict[str, str]]] = ..., data: Optional[Union[Dict[str, Union[str, Any]], bytes, IO, str]] = ..., files: Optional[Dict[str, IO]] = ...) -> Optional[RedditBase]: ...
-    def submission(self, id: Optional[str] = ..., url: Optional[str] = ...) -> Submission: ...
+    def redditor(
+        self, name: Optional[str] = ..., fullname: Optional[str] = ...
+    ) -> Redditor: ...
+    def request(
+        self,
+        method: str,
+        path: str,
+        params: Optional[Union[str, Dict[str, str]]] = ...,
+        data: Optional[
+            Union[Dict[str, Union[str, Any]], bytes, IO, str]
+        ] = ...,
+        files: Optional[Dict[str, IO]] = ...,
+    ) -> Optional[RedditBase]: ...
+    def submission(
+        self, id: Optional[str] = ..., url: Optional[str] = ...
+    ) -> Submission: ...
