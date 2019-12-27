40872c17ac57be0ce680fa27b79fb670bf604b7f
PokestarFan <sarkaraoyan@gmail.com>
2019-12-27 17:48:41 -0500

    Address black

--- praw/util/__init__.py
+++ praw/util/__init__.py
@@ -1,4 +1,4 @@
 """Package imports for utilities."""
 
-from .cache import cachedproperty # noqa: F401
-from .snake import camel_to_snake, snake_case_keys # noqa: F401
+from .cache import cachedproperty  # noqa: F401
+from .snake import camel_to_snake, snake_case_keys  # noqa: F401
