3ded2df686b5c7c8f14c5ca785b819abad9bc2e7
PokestarFan <sarkaraoyan@gmail.com>
2019-12-27 17:29:38 -0500

    Address pydocstyle

--- praw/util/__init__.py
+++ praw/util/__init__.py
@@ -1,4 +1,4 @@
 """Package imports for utilities."""
 
-from .cache import cachedproperty
-from .snake import camel_to_snake, snake_case_keys
+from .cache import cachedproperty # noqa: F401
+from .snake import camel_to_snake, snake_case_keys # noqa: F401
