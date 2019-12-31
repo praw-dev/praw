86d4f6f0b9ffbfa9ab76734c8597e88c5bb6bb8f
PokestarFan <13932583+PythonCoderAS@users.noreply.github.com>
2019-12-31 13:33:09 -0500

    New tests for flair

---
+++ tests/integration/models/reddit/test_flair.py
@@ -0,0 +1,31 @@
+"""Test praw.models.reddit.flair"""
+import mock
+
+from ... import IntegrationTest
+
+
+class TestFlair(IntegrationTest):
+    @mock.patch("time.sleep", return_value=None)
+    def test_get_compatible_flairs(self, _):
+        self.reddit.read_only = False
+        with self.recorder.use_cassette(
+            "TestFlair.test_get_AdvancedSubmissionFlair"
+        ):
+            submission = self.reddit.submission("ehswem")
+            choices = submission.flair.choices(use_flair_class=True)
+            for choice in choices:
+                advanced = choice.find_advanced_flair_template(submission)
+                assert advanced.id == choice.flair_template_id
+
+    @mock.patch("time.sleep", return_value=None)
+    def test_invalid_id(self, _):
+        self.reddit.read_only = False
+        with self.recorder.use_cassette(
+            "TestFlair.test_get_AdvancedSubmissionFlair_invalid"
+        ):
+            submission = self.reddit.submission("eh0qey")
+            choices = submission.flair.choices(use_flair_class=True)
+            choice = next(iter(choices))
+            choice.flair_template_id = "asdf"
+            advanced = choice.find_advanced_flair_template(submission)
+            assert advanced is None
