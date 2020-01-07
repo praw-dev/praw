bf040856c56bf471a8981a6754aa9f9f6b40b9bc
jarhill0 <jarhill0@users.noreply.github.com>
2020-01-07 01:25:10 -0800

    Temporarily revert "Merge pull request #1161 from PythonCoderAS/remove-unicode-strings"
    This reverts commit 3cd73529a8429864f0236873b8bcf5b761a84c37, reversing
    changes made to ff8ac5325a3d9f6c597ac9b6c444730d8a796a59.

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -23,6 +23,14 @@ class TestAPIException:
             "BAD_SOMETHING: 'invalid something' on field 'some_field'"
         )
 
+    def test_str_for_localized_error_string(self):
+        exception = APIException("RATELIMIT", u"実行回数が多すぎます", u"フィールド")
+        assert str(exception) == (
+            "RATELIMIT: '\\u5b9f\\u884c\\u56de\\u6570\\u304c\\u591a"
+            "\\u3059\\u304e\\u307e\\u3059' on field "
+            "'\\u30d5\\u30a3\\u30fc\\u30eb\\u30c9'"
+        )
+
 
 class TestClientException:
     def test_inheritance(self):
