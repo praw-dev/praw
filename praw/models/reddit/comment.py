a6adc330dff3427f493b27a28234ffc510d410c3
George Schizas <gschizas@gmail.com>
2020-03-09 18:21:23 +0200

    Dropping unnecessary result of show comment call
    If the comment doesn't exist, or there's no access etc., the standard
    exceptions will occur. If a comment is "shown" that didn't need to
    (i.e. a comment that never was collapsed, or a comment that was already
    uncollapsed) the same empty object is returned (with no effect)

--- praw/models/reddit/comment.py
+++ praw/models/reddit/comment.py
@@ -370,7 +370,5 @@ class CommentModeration(ThingModerationMixin):
            comment.mod.show()
         """
         url = API_PATH["show_comment"]
-        return (
-            self.thing._reddit.post(url, data={"id": self.thing.fullname})
-            or None
-        )
+        
+        self.thing._reddit.post(url, data={"id": self.thing.fullname})
