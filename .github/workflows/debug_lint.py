156cd062c12513549a6ffd21aa98897e34048264
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2020-04-28 22:40:53 -0400

    Add debug information to hopefully fix future problems

---
+++ .github/workflows/debug_lint.py
@@ -0,0 +1,18 @@
+"""Obtains debug information on the running system."""
+
+import os
+import sys
+
+from pip._internal.cli.main import main as pip_invoker
+
+print("Printing python version", sys.version, sep="\n")
+print("")
+for program in ("black", "flake8", "pydocstyle", "sphinx"):
+    print("Printing information on program {}".format(program))
+    pip_invoker(["show", program])
+    print("")
+print("List of python files in directory:")
+for directory, folders, files in os.walk("."):
+    for file in files:
+        if file.endswith(".py"):
+            print("{}/{}".format(directory.rstrip("/"), file))
