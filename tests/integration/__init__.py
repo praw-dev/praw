6c487a77d098bbd9db030ceebfaed739033298cd
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2020-06-25 18:31:59 -0400

    Make a builtin use_cassette function that dynamically generates cassette names

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,14 +1,19 @@
 """PRAW Integration test suite."""
+import inspect
+import logging
+import os
 
 import pytest
 import requests
-from betamax import Betamax
 
 from praw import Reddit
+from betamax import Betamax
 
 
 class IntegrationTest:
     """Base class for PRAW integration tests."""
+
+    logger = logging.getLogger(__name__)
 
     def setup(self):
         """Setup runs before all test cases."""
@@ -54,3 +59,45 @@ class IntegrationTest:
                     user_agent=pytest.placeholders.user_agent,
                     refresh_token=pytest.placeholders.refresh_token,
                 )
+
+    def use_cassette(self, cassette_name=None, **kwargs):
+        """Use a cassette. The cassette name is dynamically generated.
+
+        :param cassette_name: (Deprecated) The name to use for the cassette. All names
+            that are not equal to the dynamically generated name will be logged.
+        :param kwargs: All keyword arguments for the main function
+            (``Betamax.use_cassette``).
+        """
+        dynamic_name = self.get_cassette_name()
+        if cassette_name:
+            self.logger.debug(
+                "Static cassette name provided by {}. The following "
+                "name was provided: {}".format(dynamic_name, cassette_name)
+            )
+            if cassette_name != dynamic_name:
+                if os.environ.get("PRAWTEST_MOVE_CASSETTES"):
+                    cassettes_dir = os.path.abspath(
+                        os.path.join(__file__, "..", "cassttes")
+                    )
+                    os.rename(
+                        os.path.join(cassettes_dir, cassette_name + ".json"),
+                        os.path.join(cassettes_dir, dynamic_name + ".json"),
+                    )
+                    self.logger.warning(
+                        "Cassette {} has been moved to {}".format(
+                            cassette_name, dynamic_name
+                        )
+                    )
+                    cassette_name = None
+                else:
+                    self.logger.warning(
+                        "Dynamic cassette name for function {} does not "
+                        "match the provided cassette name: {}".format(
+                            dynamic_name, cassette_name
+                        )
+                    )
+        return self.recorder.use_cassette(cassette_name or dynamic_name, **kwargs)
+
+    def get_cassette_name(self) -> str:
+        function_name = inspect.currentframe().f_back.f_back.f_code.co_name
+        return "{}.{}".format(type(self).__name__, function_name)
