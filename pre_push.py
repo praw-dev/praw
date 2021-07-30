4cf4464b89d5d62d350ce1fd2b414decc59ac2c4
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2021-07-30 14:47:07 -0500

    Consolidate flake8 config to .flake8

--- pre_push.py
+++ pre_push.py
@@ -61,7 +61,7 @@ def run_static():
             path.join(current_directory, "tools", "check_documentation.py"),
         ]
     )
-    success &= do_process(["flake8", "--exclude=.eggs,build,docs,.venv*"])
+    success &= do_process(["flake8"])
     success &= do_process(["pydocstyle", "praw"])
     # success &= do_process(["pylint", "--rcfile=.pylintrc", "praw"])
 
