f7595a1bb90568a096b1d6e5aa7b676667ca9ffa
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2022-01-17 14:40:56 -0600

    Sort praw.models.reddit.mixins.ThingModerationMixin.distinguish arguments

--- praw/models/reddit/mixins/__init__.py
+++ praw/models/reddit/mixins/__init__.py
@@ -3,6 +3,7 @@ from json import dumps
 from typing import TYPE_CHECKING, Optional
 
 from ....const import API_PATH
+from ....util import _deprecate_args
 from .editable import EditableMixin
 from .fullname import FullnameMixin
 from .gildable import GildableMixin
@@ -66,12 +67,14 @@ class ThingModerationMixin:
         """
         self.thing._reddit.post(API_PATH["approve"], data={"id": self.thing.fullname})
 
-    def distinguish(self, how: str = "yes", sticky: bool = False):
+    @_deprecate_args("how", "sticky")
+    def distinguish(self, *, how: str = "yes", sticky: bool = False):
         """Distinguish a :class:`.Comment` or :class:`.Submission`.
 
         :param how: One of ``"yes"``, ``"no"``, ``"admin"``, or ``"special"``. ``"yes"``
             adds a moderator level distinguish. ``"no"`` removes any distinction.
-            ``"admin"`` and ``"special"`` require special user privileges to use.
+            ``"admin"`` and ``"special"`` require special user privileges to use
+            (default ``"yes"``).
         :param sticky: :class:`.Comment` is stickied if ``True``, placing it at the top
             of the comment page regardless of score. If thing is not a top-level
             comment, this parameter is silently ignored (default ``False``).
@@ -82,7 +85,7 @@ class ThingModerationMixin:
 
             # distinguish and sticky a comment:
             comment = reddit.comment("dkk4qjd")
-            comment.mod.distinguish(how="yes", sticky=True)
+            comment.mod.distinguish(sticky=True)
             # undistinguish a submission:
             submission = reddit.submission("5or86n")
             submission.mod.distinguish(how="no")
