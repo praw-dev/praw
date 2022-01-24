97cbd09305df608b2551520b0456afb8af761016
LilSpazJoekp <15524072+LilSpazJoekp@users.noreply.github.com>
2022-01-24 15:17:52 -0600

    Sort praw.models.comment_forest.CommentForest._gather_more_comments arguments

--- praw/models/comment_forest.py
+++ praw/models/comment_forest.py
@@ -18,7 +18,7 @@ class CommentForest:
     """
 
     @staticmethod
-    def _gather_more_comments(tree, parent_tree=None):
+    def _gather_more_comments(tree, *, parent_tree=None):
         """Return a list of :class:`.MoreComments` objects obtained from tree."""
         more_comments = []
         queue = [(None, x) for x in tree]
@@ -185,7 +185,9 @@ class CommentForest:
                 remaining -= 1
 
             # Add new MoreComment objects to the heap of more_comments
-            for more in self._gather_more_comments(new_comments, self._comments):
+            for more in self._gather_more_comments(
+                new_comments, parent_tree=self._comments
+            ):
                 more.submission = self._submission
                 heappush(more_comments, more)
             # Insert all items into the tree
